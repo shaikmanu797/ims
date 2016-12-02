c2cfdf8d632de87926b34e55e21a0ed502e9e9a9
Mansoor Baba Shaik <shaikmanu797@gmail.com>
2016-12-01 23:11:12 -0500

    sidebar icon change

--- view/sidebar.php
+++ view/sidebar.php
@@ -1,7 +1,7 @@
 <div class="icon-bar">
     <a class="active" href="#"><i class="fa fa-home"></i></a>
     <a href="#"><i class="fa fa-search"></i></a>
-    <a href="#"><i class="fa fa-envelope"></i></a>
-    <a href="#"><i class="fa fa-globe"></i></a>
+    <a href="#"><i class="fa fa-plus"></i></a>
+    <a href="#"><i class="fa fa-pencil-square-o"></i></a>
     <a href="#"><i class="fa fa-trash"></i></a>
 </div>
