7d56c29dfe6c8edb63f0d3a2c3b8c30a1db0d83f
Gaurav Samant <samantgau@gmail.com>
2016-12-02 16:46:32 -0500

    location page

--- model/addLocation.php
+++ model/addLocation.php
@@ -3,6 +3,22 @@
 Mansoor Baba Shaik
 */
 
-echo "Loc Page";
-
+/*echo "Loc Page";*/
 ?>
+<br/>
+<form name="addlocation" action="" method="post">
+  <!-- Table goes in the document BODY -->
+  <table class="table-style-three">
+      <thead>
+      <!-- Display CRUD options in TH format -->
+
+
+      <tr>
+        <th>Location Name:</th>
+        <td><input type="text" name="locationName" value="" maxlength="250" size="100" required></td>
+
+          <td>&nbsp;<input type="submit" name="Submit" value="Create"></td>
+      </tr>
+      </thead>
+  </table>
+</form>
