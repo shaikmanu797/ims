be7328f8be4fa11f18280f12f846a546adbdceea
Mansoor Baba Shaik <shaikmanu797@gmail.com>
2016-12-05 12:05:23 -0500

    no message

--- view/sidebar.php
+++ view/sidebar.php
@@ -1,5 +1,5 @@
 <div class="icon-bar">
-    <a class="active" href="#"><i class="fa fa-home"></i></a>
+    <a href="home"><i class="fa fa-home"></i></a>
     <a href="#"><i class="fa fa-search"></i></a>
     <a href="create"><i class="fa fa-plus"></i></a>
     <a href="#"><i class="fa fa-pencil-square-o"></i></a>
